0385ca9ae8cc07625900ae2ec9dfe7332b7bc20b
Brandt Lareau <brandt.lareau@gmail.com>
2018-11-07 19:16:40 -0800

    Added more details to the website
    Added more details to the website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   respond_to :html, :json
+
+  # this render dynamic / static data for
+  def pages
+    respond_to do |format|
+      format.html { render "pages/#{params[:page_name]}" }
+    end
+  end
 end
